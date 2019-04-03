2a9c1fba3fa902cdf2011aea5a94777b059e9ec3
zaggnut <laskowskiski@gmail.com>
2019-04-02 21:24:11 -0700

    added multiple (incomplete) pages to app and to Master Detail Page menu.  Created and added app logo

--- beach_day/beach_day/MainPage.xaml.cs
+++ beach_day/beach_day/MainPage.xaml.cs
@@ -15,5 +15,26 @@ namespace beach_day
             Detail = new NavigationPage(new BeachSelectPage()); //change this the first page you want the user to see
 
         }
+
+        private void BeachSelect_Creation_Button(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new BeachSelectPage());
+            IsPresented = false; //hides the MainPage (master detail page)
+        }
+        private void BeachItemChecklist_Creation_Button(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ItemChecklist());
+            IsPresented = false;
+        }
+        private void BeachFacts_Creation_Button(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new BeachFacts());
+            IsPresented = false;
+        }
+        private void About_Creation_Button(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new About());
+            IsPresented = false;
+        }
     }
 }
